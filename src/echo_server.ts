7b794ea07edbf3818ea0a773a1e41c6f5f596d5c
tudordaian <tudor.daian@student.tuiasi.ro>
2025-08-13 11:33:33 +0300

    promise based echo server

--- src/echo_server.ts
+++ src/echo_server.ts
@@ -1,26 +1,115 @@
 import * as net from "net"
 
+// API Promise-based pentru socket-uri TCP
+type TCPConn = {
+    socket: net.Socket
+    err: null | Error   // 'error' event
+    ended: boolean      // 'end' event
+    reader: null | {    // callback-urile promise-ului read-ului curent
+        resolve: (value: Buffer) => void
+        reject: (reason: Error) => void
+    }
+}
 
-function newConn(socket: net.Socket): void {
-    console.log(`New connection - remote address: ${socket.remoteAddress}, remote port: ${socket.remotePort} `)
-
+function soInit(socket: net.Socket): TCPConn {
+    const conn: TCPConn = {
+        socket: socket, err: null, ended: false, reader: null
+    };
     socket.on('data', (data: Buffer) => {
-        console.log('Data: ' + data)
-        socket.write(data)
-
-        if(data.includes('q')) {
-            console.log('Closing connection.')
-            socket.end()    // trimite FIN
+        console.assert(conn.reader)
+        // pauza la eventul 'data' pana la urmatorul read
+        conn.socket.pause()
+        // incheie promise-ul read-ului curent
+        conn.reader!.resolve(data)
+        conn.reader = null
+    });
+    socket.on('end', () => {
+        // incheie promise-ul read-ului curent
+        conn.ended = true
+        if(conn.reader) {
+            conn.reader.resolve(Buffer.from(''))
+            conn.reader = null
+        }
+    });
+    socket.on('error', (err: Error) => {
+        conn.err = err
+        if(conn.reader) {
+            conn.reader.reject(err)
+            conn.reader = null
         }
     });
 
-    socket.on('end', () => {    // cand primeste FIN
-        console.log('EOF.')
-    });
+    return conn
+}
+
+function soRead(conn: TCPConn): Promise<Buffer> {
+    console.assert(!conn.reader)    // fara call-uri concurente
+    return new Promise((resolve, reject) => {
+        // daca conexiunea nu e readable, finalizeaza promise-ul acum
+        if(conn.err) {
+            reject(conn.err)
+            return
+        }
+        if(conn.ended) {
+            resolve(Buffer.from(''))    // EOF
+            return
+        }
+
+        // salvare callback-uri ale promise-ului
+        conn.reader = { resolve: resolve, reject: reject }
+        // resume la 'data' pentru a finaliza promise-ul mai tarziu
+        conn.socket.resume()
+    })
+}
+
+function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
+    console.assert(data.length > 0)
+    return new Promise((resolve, reject) => {
+        if(conn.err) {
+            reject(conn.err)
+            return
+        }
+
+        conn.socket.write(data, (err?: Error | null) => {
+            if(err) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
+    })
+}
+
+async function serveClient(socket: net.Socket): Promise<void> {
+    const conn: TCPConn = soInit(socket)
+    while(true) {
+        const data = await soRead(conn)
+        if(data.length === 0) {
+            console.log('connection ended')
+            break
+        }
+
+        console.log('buffer data:', data)
+        console.log('data as string:', JSON.stringify(data.toString('utf-8')))
+        await soWrite(conn, data)
+    }
+}
+
+async function newConn(socket: net.Socket): Promise<void> {
+    console.log(`New connection - remote address: ${socket.remoteAddress}, remote port: ${socket.remotePort} `)
+    try {
+        await serveClient(socket)
+    } catch(exc) {
+        console.error('exception: ', exc)
+    } finally {
+        socket.destroy()
+    }
 }
 
 
-let server = net.createServer()
+let server = net.createServer({
+    pauseOnConnect: true    // previne 'data' loss
+})
 server.on('connection', newConn)
 server.on('error', (err: Error)=> { throw err } )
 
